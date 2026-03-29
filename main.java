faa4d24fc353d5ead828cb0febd829991438513a
Muzammil Shaikh <smuzammil2508@gmail.com>
2026-03-29 15:07:11 +0530

    Takes input from user to insert into DB and use Batch Processing to add multiple values at once

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 import java.sql.*; // IMPORTING EVERYTHING INSIDE THE SQL PACKAGE
+import java.util.*; // IMPORTING EVERYTHING INSIDE THE UTIL PACKAGE
 
 public class main {
     private static final String url = "jdbc:mysql://localhost:3306/"; // URL TO CONNECT WITH MYSQL DATABASE
@@ -6,6 +7,8 @@ public class main {
     private static final String password = "mysqlpassword999@"; // PASSWORD FOR THE DATABASE
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         // STEP 1 : CONNECT IDE TO DATABSE
         // we downloaded the mysqlconnector j from browser and added it to library
@@ -49,16 +52,66 @@ public class main {
             String table_query = "CREATE TABLE IF NOT EXISTS students ( id INT PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL , age INT NOT NULL , roll INT NOT NULL)";
             st.executeUpdate(table_query);
 
+            // =================================================================================================================================
+
             // ADDING DUMMY VALUES IN DATA BASE USING PREPARED STATEMENT
-            String insert_query = "INSERT INTO students(name,age,roll) VALUES(?,?,?)";
-            PreparedStatement ps = conn.prepareStatement(insert_query);
-            ps.setString(1, "Pranav");
-            ps.setInt(2, 19);
-            ps.setInt(3, 54);
-            int a1 = ps.executeUpdate();
-            if (a1 != 0) {
-                System.out.println("value added in students database");
+
+            // TABLE WITH FIXED VALUES
+
+            // String insert_query = "INSERT INTO students(name,age,roll) VALUES(?,?,?)";
+            // PreparedStatement ps = conn.prepareStatement(insert_query);
+            // ps.setString(1, "Pranav");
+            // ps.setInt(2, 19);
+            // ps.setInt(3, 54);
+            // int a1 = ps.executeUpdate();
+            // if (a1 != 0) {
+            // System.out.println("value added in students database");
+            // }
+
+            // ------------------------------------------------------------------------------------------------------------------------------------------
+
+            // TABLE TAKING INPUT FROM USER AND USING BATCH PROCESSING
+
+            String query_insert = "INSERT INTO students(name,age,roll) VALUES(?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(query_insert);
+            while (true) {
+
+                System.out.println("enter name :");
+                String s = sc.next();
+                System.out.println("enter age :");
+                int a = sc.nextInt();
+                System.out.println("enter roll :");
+                int r = sc.nextInt();
+                System.out.print("do you want to add more (Y/N) :");
+                String choice = sc.next();
+
+                ps.setString(1, s);
+                ps.setInt(2, a);
+                ps.setInt(3, r);
+                ps.addBatch();
+
+                if (choice.toUpperCase().equals("N")) {
+                    break;
+                }
             }
+            // BATCH PROCESSING
+            int[] batchResults = ps.executeBatch();
+            for (int i = 0; i < batchResults.length; i++) {
+                System.out.printf("Row %d: %d changes made%n", i + 1, batchResults[i]);
+            }
+
+            // ===================================================================================================================================
+
+            // DELETING VALUES IN DATABASE USING PREPARED STATEMENTS
+
+            // String delete_query = " DELETE FROM students WHERE id = ?";
+            // PreparedStatement ps1 = conn.prepareStatement(delete_query);
+            // ps1.setInt(1, 1);
+            // int a2 = ps1.executeUpdate();
+            // if (a2 != 0) {
+            // System.out.println("value added in students database");
+            // }
+
         } catch (SQLException e) {
             System.out.println("error : " + e.getMessage());
 
